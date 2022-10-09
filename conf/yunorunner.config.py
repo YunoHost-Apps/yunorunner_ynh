fd8c39fd519c883093fb40f6b58f2af2f4995ebb
yalh76 <yalh@yahoo.com>
2022-10-09 17:03:25 +0200

    Implement ci_package_check_path and ci_package_check_datadir

--- conf/yunorunner.config.py
+++ conf/yunorunner.config.py
@@ -1,7 +1,7 @@
 BASE_URL = "https://__DOMAIN____PATH__"
 PORT = __PORT__
 DEBUG = False
-PATH_TO_ANALYZER = "/home/CI_package_check/analyseCI.sh"
+PATH_TO_ANALYZER = "__CI_PACKAGE_CHECK_PATH__/analyseCI.sh"
 MONITOR_APPS_LIST = False
 MONITOR_GIT = False
 MONITOR_ONLY_GOOD_QUALITY_APPS = False
