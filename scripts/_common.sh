2783d6af2036fb3f581809ca815518d95cbb127d
Félix Piédallu <felix@piedallu.me>
2024-11-09 17:05:23 +0100

    Move incus configuration to the incus package

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -42,33 +42,10 @@ tweak_yunohost() {
 }
 
 setup_incus() {
-    ynh_print_info "Configuring Incus..."
-
     # ci_user will be the one launching job, gives it permission to run incus commands
     usermod -a -G incus-admin "$app"
 
-    if [ "$cluster" -eq 1 ]; then
-        yunohost firewall allow TCP 8443
-
-        free_space=$(df --output=avail / | sed 1d)
-        btrfs_size=$(( free_space * 90 / 100 / 1024 / 1024 ))
-        incus_network=$((1 + RANDOM % 254))
-        ynh_add_config --template="incus-preseed.yml" --destination="$install_dir/incus-preseed.yml"
-        incus admin init --preseed < "$install_dir/incus-preseed.yml"
-        rm "$install_dir/incus-preseed.yml"
-
-        incus config set core.https_address "[::]"
-    else
-        incus admin init --auto # --storage-backend=dir
-    fi
-
     ynh_exec_as "$app"  incus remote add yunohost https://repo.yunohost.org/incus --protocol simplestreams --public
-}
-
-exposed_ports_if_cluster() {
-    if [ "$cluster" -eq 1 ]; then
-        echo "--needs_exposed_ports=8443"
-    fi
 }
 
 #=================================================
